9b02f468f9772afa108632f760e4aaf850b710e0
ZuTa <zuta.developer@gmail.com>
2014-08-03 17:28:37 +0300

    test6

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from bottle import default_app, route, redirect, run, request
 
 APP_ID = '4488301'
-AUTH_URL = 'https://oauth.vk.com/authorize?client_id=' + APP_ID + '&scope=4&redirect_uri=http://zuta.pythonanywhere.com/start&response_type=code&v=5.23'
+AUTH_URL = 'https://oauth.vk.com/authorize?client_id=' + APP_ID + '&scope=4108&redirect_uri=http://zuta.pythonanywhere.com/start&response_type=code&v=5.23'
 
 @route('/')
 def login():
