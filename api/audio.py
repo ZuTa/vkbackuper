a259ef03baae1617188945c8b4c64bfb63fbbb59
ZuTa <zuta.developer@gmail.com>
2014-09-11 00:09:40 +0300

    add get_all_audio

--- api/audio.py
+++ api/audio.py
@@ -3,7 +3,10 @@ import common
 from models.audio import Audio, AudioAlbum
 
 
+MAX_AUDIO_TO_RETURN = 200
 GET_AUDIO_COUNT = "audio.getCount"
+GET_AUDIO = "audio.get"
+GET_ALBUMS= "audio.getAlbums"
 
 def get_audio_count(access_token, user_id):
     url = common.create_method_url(GET_AUDIO_COUNT, access_token, owner_id=user_id)
@@ -11,3 +14,52 @@ def get_audio_count(access_token, user_id):
     res = common.make_request(url)
 
     return int(res['response'])
+
+def parse_audio(items, callback):
+    for item in items:
+        url = item['url'].replace(".vk.me/", ".vk.com/")
+
+        album_id = item.get('album_id', 0)
+
+        callback(Audio(int(item['id']), album_id, item['artist'], item['title'], url))
+
+def retrieve_audio_albums(access_token):
+    albums = {}
+    offset = 0
+    while True:
+        url = common.create_method_url(GET_ALBUMS, access_token, count=MAX_AUDIO_TO_RETURN, offset=offset)
+        json = common.make_method_request(url)
+
+        items = json['items']
+        for item in items:
+            album = AudioAlbum(int(item['id']), item['title'])
+
+            albums[album.uid] = album
+
+        offset += len(json['items'])
+        if int(json['count']) - offset == 0:
+            break
+
+    return albums
+
+def get_all_audio(access_token):
+    result = []
+    offset = 0
+    while True:
+        url = common.create_method_url(GET_AUDIO, access_token, count=MAX_AUDIO_TO_RETURN, offset=offset)
+        json = common.make_method_request(url)
+
+        parse_audio(json['items'], result.append)
+
+        offset += len(json['items'])
+        if int(json['count']) - offset == 0:
+            break
+
+    albums = retrieve_audio_albums(access_token, album_ids)
+
+    # inject albums into audio objects
+    for audio in result:
+        if audio.album_id != 0:
+            audio.album = albums[audio.album_id]
+
+    return result
--- models/audio.py
+++ models/audio.py
@@ -6,13 +6,21 @@ class AudioAlbum(object):
     @property
     def title(self):
         return self._title
-    
+
 
 class Audio(object):
-    def __init__(self, _id, artist, title, url, album_id):
+    def __init__(self, _id, album_id, artist, title, url):
         self._id = _id
         self._artist = artist
-        self._title = title 
+        self._title = title
         self._url = url
         self._album_id = album_id
-        
+        self._album = None
+
+    @property
+    def album(self):
+        return self._album
+
+    @album.setter
+    def album(self, value):
+        self._album = value
