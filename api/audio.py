c20204daf5e3913e74e72f6fd56bbc3d8d860d96
ZuTa <zuta.developer@gmail.com>
2014-09-10 23:37:36 +0300

    fix

--- api/audio.py
+++ api/audio.py
@@ -5,8 +5,8 @@ from models.audio import Audio, AudioAlbum
 
 GET_AUDIO_COUNT = "audio.getCount"
 
-def get_audio_count(access_token):
-    url = common.create_method_url(GET_ALBUMS_COUNT, access_token)
+def get_audio_count(access_token, user_id):
+    url = common.create_method_url(GET_ALBUMS_COUNT, access_token, owner_id=)
 
     res = common.make_request(url)
 
